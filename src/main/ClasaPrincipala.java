d0413091b6d4fed5cded4065868cb60109c0de8f
Diana <furcicdiana1993@gmail.com>
2020-02-01 00:11:01 +0200

    v

--- src/main/ClasaPrincipala.java
+++ src/main/ClasaPrincipala.java
@@ -3,8 +3,19 @@ package main;
 public class ClasaPrincipala {
 
 	public static void main(String[] args) {
-		System.out.println("Hello World!");
-
+		
+		//int a=3;
+		//int b=(a=2)*a; // ia valoarea lui a din paranteza 
+		//int c=b*(b=5); // ia valoarea lui b din paranteza nu cea calculata mai sus
+		//System.out.println("a=" +a+",b="+b+",c="+c);
+	
+		
+ 
+ double d=2.95;
+ int i=4;
+ System.out.println(++d>i?d:i); // ia doar valoarea lui i
+ }
+		 
 	}
 
-}
+
